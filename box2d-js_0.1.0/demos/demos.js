64903ceb0f23c27fbd31c67be0761a3a3f43581e
eranws <eranws@gmail.com>
2013-06-16 16:19:17 +0300

    bla

--- box2d-js_0.1.0/demos/demos.js
+++ box2d-js_0.1.0/demos/demos.js
@@ -33,6 +33,25 @@ Event.observe(window, 'load', function() {
 	canvasHeight = parseInt(canvasElm.height);
 	canvasTop = parseInt(canvasElm.style.top);
 	canvasLeft = parseInt(canvasElm.style.left);
+	
+	
+
+    Event.observe(document, 'mousemove', function(event){console.log(event);$('mouser').value = "X: " + Event.pointerX(event) + "px Y: " + Event.pointerY(event) + "px";});
+
+	Event.observe(document,'keyup',
+		function(event){
+		console.log(event);
+		
+		console.log("a".charCodeAt(0));
+		
+			if(event.keyCode == Event.KEY_LEFT || event.keyCode == Event.KEY_RIGHT
+			|| event.keyCode == (1<<5 | "a".charCodeAt(0)) || event.keyCode == "Z")
+			{
+				alert('Tab Pressed' + event.keyCode);
+			}
+		}
+	);
+
 	Event.observe('canvas', 'click', function(e) {
 		//setupNextWorld();
 		if (Math.random() < 0.5) 
